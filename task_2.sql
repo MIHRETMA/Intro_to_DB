6f50a502c2706321f899949ccd087dd937d4ac47
Mihret Mekbebe Asamenew <mihret.mekbebe@berhan-bank.local>
2025-06-28 13:03:20 +0300

    SQL server

--- task_2.sql
+++ task_2.sql
@@ -1,42 +1,52 @@
 USE alx_book_store;
 
-CREATE TABLE AUTHORS (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    bio TEXT
-);
+CREATE TABLE Books
+(
+book_id INT PRIMARY KEY,
+title VARCHAR(130),
+author_id INT,
+price DOUBLE,
+publication_date DATE,
+FOREIGN KEY (author_id) REFERENCES Authors(author_id))
+;
 
-CREATE TABLE BOOKS (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
-);
 
-CREATE TABLE CUSTOMERS (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50),
-    email VARCHAR(100) UNIQUE,
-    phone VARCHAR(20),
-    address TEXT
-);
+CREATE TABLE Authors
+(
+author_id INT PRIMARY KEY,
+author_name VARCHAR(215))
+;
 
-CREATE TABLE ORDERS (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
-);
 
-CREATE TABLE ORDER_DETAILS (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity INT,
-    price DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
-);
+CREATE TABLE Customers
+(
+customer_id INT PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+)
+;
+
+
+
+CREATE TABLE Orders
+(
+order_id INT PRIMARY KEY,
+customer_id INT,
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+)
+;
+
+
+
+CREATE TABLE Order_Details
+(
+orderdetailid INT PRIMARY KEY ,
+order_id INT,
+book_id INT,
+quantity DOUBLE,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)
+;
